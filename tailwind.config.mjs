fa6da77d2cc4728adcf84058bfd024d1a3f10d9f
Roger Sancho <rogersanchom@gmail.com>
2024-01-27 11:33:45 +0100

    add - open graph metadata

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,20 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: {
+          1: "hsl(001 80% 60%)",
+          2: "hsl(100 90% 80%)",
+        },
+        bkg: "hsl(210 40% 98%)",
+        content: "hsl(217 30% 15%)",
+      },
+      animation: {
+        "spin-slower": "spin 35s ease infinite",
+        "spin-slow": "spin 25s ease-in-out infinite reverse",
+      },
+    },
   },
   plugins: [],
 };
